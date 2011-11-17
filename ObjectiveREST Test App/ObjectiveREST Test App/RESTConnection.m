11e9ac3eb137e91a8b12f299815f49ac8002c352
Yoann Gini <yoann.gini@gmail.com>
2011-11-17 21:22:12 +0100

    REST service is working with static content. Client is able to chose format between json, plist and bplist by the Accept header.

--- ObjectiveREST Test App/ObjectiveREST Test App/RESTConnection.m
+++ ObjectiveREST Test App/ObjectiveREST Test App/RESTConnection.m
@@ -9,13 +9,54 @@
 #import "RESTConnection.h"
 #import "HTTPLogging.h"
 #import "HTTPDataResponse.h"
+#import "HTTPMessage.h"
+#import "GCDAsyncSocket.h"
+#import "NSObject+SBJson.h"
 
 // Log levels: off, error, warn, info, verbose
 // Other flags: trace
 static const int httpLogLevel = HTTP_LOG_LEVEL_WARN; // | HTTP_LOG_FLAG_TRACE;
 
+#define TIMEOUT_WRITE_ERROR					30
+#define HTTP_FINAL_RESPONSE					91
+
+#define SUPPORTED_CONTENT_TYPE				[NSArray arrayWithObjects:@"application/x-bplist", @"application/x-plist", @"application/json", nil]
+
 @implementation RESTConnection
 
+- (NSData*)preparedResponseFromDictionary:(NSDictionary*)dict withContentType:(NSString*)ContentType {
+	NSString *errorString = nil;
+	
+	if ([ContentType isEqualToString:@"application/x-bplist"]) 
+		return [NSPropertyListSerialization dataFromPropertyList:dict format:NSPropertyListBinaryFormat_v1_0 errorDescription:&errorString];
+	else if ([ContentType isEqualToString:@"application/x-plist"]) 
+		return [NSPropertyListSerialization dataFromPropertyList:dict format:NSPropertyListXMLFormat_v1_0 errorDescription:&errorString];
+	else if ([ContentType isEqualToString:@"application/json"]) 
+		return [[dict JSONRepresentation] dataUsingEncoding:NSUTF8StringEncoding];
+	else
+		return nil;
+}
+
+/**
+ * Called if the client ask for a unavaiable content type
+ **/
+- (void)handleOptionNotImplemented
+{
+	// Override me for custom error handling of 404 not found responses
+	// If you simply want to add a few extra header fields, see the preprocessErrorResponse: method.
+	// You can also use preprocessErrorResponse: to add an optional HTML body.
+	
+	HTTPLogInfo(@"HTTP Server: Error 501 - Not Implemented (%@)", [self requestURI]);
+	
+	// Status Code 404 - Not Found
+	HTTPMessage *response = [[HTTPMessage alloc] initResponseWithStatusCode:501 description:nil version:HTTPVersion1_1];
+	[response setHeaderField:@"Content-Length" value:@"0"];
+	
+	NSData *responseData = [self preprocessErrorResponse:response];
+	[asyncSocket writeData:responseData withTimeout:TIMEOUT_WRITE_ERROR tag:HTTP_FINAL_RESPONSE];
+	
+	[response release];
+}
 
 - (BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path
 {
@@ -51,10 +92,36 @@ static const int httpLogLevel = HTTP_LOG_LEVEL_WARN; // | HTTP_LOG_FLAG_TRACE;
 	
 #warning Need to handle message to know if it's GET / POST / PUT / DELETE and path to understand what we speaking for.
 	
-	NSData *response = nil;
-	response = [@"<html><body>Correct<body></html>" dataUsingEncoding:NSUTF8StringEncoding];
-
-	return [[[HTTPDataResponse alloc] initWithData:response] autorelease];
+	NSArray *acceptedContentType = [[request headerField:@"Accept"] componentsSeparatedByString:@","];
+	NSMutableArray *retainedContentType = [NSMutableArray new];
+	
+	for (NSString *contentType in SUPPORTED_CONTENT_TYPE) {
+		if ([acceptedContentType containsObject:contentType]) [retainedContentType addObject:contentType];
+	}
+	
+	if ([retainedContentType count] == 0) { 
+		[self handleOptionNotImplemented];		
+	} else {
+		
+		NSString *ContentType = [[retainedContentType objectAtIndex:0] retain];
+		[retainedContentType release];
+		
+		if ([method isEqualToString:@"GET"]) {
+			
+			return [[[HTTPDataResponse alloc] initWithData:[self preparedResponseFromDictionary:[NSDictionary dictionaryWithObjectsAndKeys:@"foo", @"bar", @"alice", @"bob", nil]
+																				withContentType:ContentType]] 
+					autorelease];
+		} else if ([method isEqualToString:@"POST"]) {
+			
+		} else if ([method isEqualToString:@"PUT"]) {
+			
+		} else if ([method isEqualToString:@"DELETE"]) {
+			
+		} else [self handleUnknownMethod:method];
+		
+		[ContentType release];
+	}
+	return [super httpResponseForMethod:method URI:path];
 }
 
 - (void)prepareForBodyWithSize:(UInt64)contentLength
